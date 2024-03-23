4f00d1dd329913b4e0887220aca4387c5f2a9fe9
Dmitriy Dmitriev <dmitriyd1107@gmail.com>
2024-03-23 15:42:30 +0700

    final change

--- src/components/RemainsPage.jsx
+++ src/components/RemainsPage.jsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react';
 import cl from '../styles/RemainsPage.module.css';
 import RemainsTable from './RemainsTable';
 import { Link } from 'react-router-dom';
@@ -6,46 +5,15 @@ import { useUserStore } from '../store/user';
 import { useGoodsStore } from '../store/goodsValue';
 
 const RemainsPage = () => {
-	const goods = useRef([
-		{
-			productName: 'Курица гриль',
-			ingridient1: 'Бедро',
-			ingridient2: 'Филе Грудки',
-			quantity: 66,
-			salesForCurrentDay: 54,
-			currentBalance: 170,
-			id: 0,
-		},
-		{
-			productName: 'Шашлык',
-			ingridient1: 'Свинина',
-			ingridient2: 'Говядина',
-			quantity: 23,
-			salesForCurrentDay: 42,
-			currentBalance: 100,
-			id: 1,
-		},
-		{
-			productName: 'Овощи гриль',
-			ingridient1: 'Баклажаны',
-			ingridient2: 'Кабачки',
-			quantity: 15,
-			salesForCurrentDay: 20,
-			currentBalance: 60,
-			id: 2,
-		},
-	]);
+	const { goodsData, setGoodsData } = useGoodsStore();
 
 	const { userData } = useUserStore();
-	const { setGoodsData } = useGoodsStore();
 
 	const handleQuantity = (productName, quantity) => {
-		const exist = goods?.current?.findIndex(
-			(i) => i.productName === productName
-		);
+		const exist = goodsData.findIndex((i) => i.productName === productName);
 		if (exist !== -1) {
-			goods.current[exist].quantity += quantity;
-			setGoodsData(goods);
+			goodsData[exist].quantity += quantity;
+			setGoodsData(goodsData);
 		}
 	};
 
@@ -72,7 +40,7 @@ const RemainsPage = () => {
 						<th className={cl.remains_th}>Текущий остаток</th>
 					</tr>
 				</thead>
-				{goods?.current?.map((i) => (
+				{goodsData.map((i) => (
 					<RemainsTable
 						productName={i.productName}
 						ingridient1={i.ingridient1}
--- src/store/goodsValue.js
+++ src/store/goodsValue.js
@@ -1,7 +1,35 @@
 import { create } from 'zustand';
 
 export const useGoodsStore = create((set) => ({
-	goodsData: null,
+	goodsData: [
+		{
+			productName: 'Курица гриль',
+			ingridient1: 'Бедро',
+			ingridient2: 'Филе Грудки',
+			quantity: 66,
+			salesForCurrentDay: 54,
+			currentBalance: 170,
+			id: 0,
+		},
+		{
+			productName: 'Шашлык',
+			ingridient1: 'Свинина',
+			ingridient2: 'Говядина',
+			quantity: 23,
+			salesForCurrentDay: 42,
+			currentBalance: 100,
+			id: 1,
+		},
+		{
+			productName: 'Овощи гриль',
+			ingridient1: 'Баклажаны',
+			ingridient2: 'Кабачки',
+			quantity: 15,
+			salesForCurrentDay: 20,
+			currentBalance: 60,
+			id: 2,
+		},
+	],
 	setGoodsData: (data) => {
 		set({ goodsData: data });
 	},
